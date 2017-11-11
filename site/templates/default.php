4368415599f4b871ab57b53dfa3cc9d9e7cccb02
Julio Cesar Montenegro <creativo@686studio.com>
2017-11-11 14:32:40 -0800

    added content tex

--- site/templates/default.php
+++ site/templates/default.php
@@ -4,10 +4,12 @@
 	<div class="o-flex">
 
 	<?php snippet('sidebar') ?>
-
+	
 	<div class="o-content u-px2 u-py2">
-		<h1><?= $page->title() ?></h1>
-		<hr class="u-mb2">
+		<?php if(!$page->isHomePage()): ?>
+			<h1><?= $page->title() ?></h1>
+			<hr class="u-mb2">
+		<?php endif; ?>
 		<?= $page->text()->kirbytext() ?>
 		<?php foreach(page()->children()->visible()->limit(null) as $item): ?>
 			<h2 id="<?= $item->title() ?>"><?= $item->title() ?></h2>
